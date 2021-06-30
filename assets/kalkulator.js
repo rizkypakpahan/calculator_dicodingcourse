98d1b5cde33162381d62c71e0a6a550c652f08ad
rizkypakpahan <rizky.kurniawanpakpahan@gmail.com>
2021-06-30 11:02:11 +0700

    function for opeator

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -11,25 +11,9 @@ function updateDisplay() {
 
 function clearCalculator() {
   calculator.displayNumber = "0";
-  (calculator.operator = null),
-    (calculator.firstNumber = null),
-    (calculator.waitingForSecondNumber = false);
-}
-
-function inputDigit(digit) {
-  calculator.displayNumber += digit;
-}
-
-const buttons = document.querySelectorAll(".button");
-
-for (let button of buttons) {
-  button.addEventListener("click", function (event) {
-    // mendapatkan objek elemen yang diklik
-    const target = event.target;
-
-    inputDigit(target.innerText);
-    updateDisplay();
-  });
+  calculator.operator = null;
+  calculator.firstNumber = null;
+  calculator.waitingForSecondNumber = false;
 }
 
 function inputDigit(digit) {
@@ -40,33 +24,72 @@ function inputDigit(digit) {
   }
 }
 
-button.addEventListener("click", function (event) {
-  // mendapatkan objek elemen yang diklik
-  const target = event.target;
+function inverseNumber() {
+  if (calculator.displayNumber === "0") {
+    return;
+  }
+  calculator.displayNumber = calculator.displayNumber * -1;
+}
 
-  if (target.classList.contains("clear")) {
-    clearCalculator();
+function handleOperator(operator) {
+  if (!calculator.waitingForSecondNumber) {
+    calculator.operator = operator;
+    calculator.waitingForSecondNumber = true;
+    calculator.firstNumber = calculator.displayNumber;
+    calculator.displayNumber = "0";
+  } else {
+    alert("Operator sudah ditetapkan");
+  }
+}
+
+function performCalculation() {
+  if (calculator.firstNumber == null || calculator.operator == null) {
+    alert("Anda belum menetapkan operator");
+    return;
+  }
+
+  let result = 0;
+  if (calculator.operator === "+") {
+    result =
+      parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+  } else {
+    result =
+      parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+  }
+
+  calculator.displayNumber = result;
+}
+
+const buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+  button.addEventListener("click", function (event) {
+    // mendapatkan objek elemen yang diklik
+    const target = event.target;
+
+    if (target.classList.contains("clear")) {
+      clearCalculator();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("negative")) {
+      inverseNumber();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("equals")) {
+      performCalculation();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("operator")) {
+      handleOperator(target.innerText);
+      return;
+    }
+
+    inputDigit(target.innerText);
     updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains("negative")) {
-    inverseNumber();
-    updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains("equals")) {
-    performCalculation();
-    updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains("opertor")) {
-    handleOperator(target.innerText);
-    return;
-  }
-
-  inputDigit(target.innerText);
-  updateDisplay();
-});
+  });
+}
